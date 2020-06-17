b3a7be6e6e90753337e2e853f97aa52b8ef5e3a1
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-06-17 14:38:29 +0300

    lab-18

--- caps/caps.js
+++ caps/caps.js
@@ -1,38 +1,37 @@
-const events = require('../events.js');
-require('./driver.js');
-const vendor = require('../vendor.js');
-const net = require('net');
+'use strict';
+const io=require('socket.io')('3000');
 
-const PORT = process.env.PORT || 3000;
-
-const server = net.createServer();
-server.listen(PORT, () => {
-  console.log(`Listenning on  Port${PORT}`);
+io.on('connection',(socket)=>{
+  console.log('CORE',socket.id);
 });
 
+const caps=io.of('/caps');
+caps.on('connection',(socket)=>{
+  console.log('Connected',socket.id);
+
+  socket.on('join', room=>{
+    console.log('registered as', room);
+    socket.join(room);
+  });
+
+  socket.on('pickup', (payload)=>{
+    logIt('pickup',payload);
+    caps.emit('pickup',payload);
+  });
 
 
+  socket.on('in-transit',(payload)=>{
+    logIt('in-transit', payload);
+    caps.to(payload.store).emit('in-transit',payload);
+  });
 
+  socket.on('delivered',(payload)=>{
+    logIt('delivered', payload);
+    caps.to(payload.store).emit('delivered',payload);
+  });
+});
 
-function readyForPickup(){
-  setTimeout(() => {
-    let fakeOrder = vendor();
-    
-    setTimeout(() => {
-      events.emit('pickup',fakeOrder); 
-      console.log(`DRIVER: picked up ${fakeOrder.orderID}`);
-      
-    }, 1000);
-    
-    setTimeout(() => {
-      events.emit('in-transit', fakeOrder );
-      console.log(`DRIVER: delivered up ${fakeOrder.orderID}`);
-      console.log(`VENDOR: Thank you for delivering ${fakeOrder.orderID}`); 
-      events.emit('delivered',fakeOrder); 
-    }, 3000),
-    
-    
-    readyForPickup();
-  }, 5000); 
+function logIt(event,payload){
+  let time=new Date();
+  console.log({time,event,payload});
 }
-readyForPickup();
